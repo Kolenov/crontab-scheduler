2007c9775c0285d1733fe20ac1679141b82d93d2
Eduard Kolenov <kolenov.ed@gmail.com>
2018-10-12 01:34:44 +0300

    add config

---
+++ src/components/Scheduler/config.js
@@ -0,0 +1,14 @@
+export const COMPONENTS = {
+  dateTime: 1,
+  weekDays: 1 << 1,
+  months: 1 << 2,
+  monthDays: 1 << 3,
+};
+
+export const COMPONENT_STATE = {
+  once: 0,
+  everyDay: COMPONENTS.dateTime,
+  everyWeek: COMPONENTS.dateTime | COMPONENTS.weekDays,
+  everyMonth: COMPONENTS.dateTime | COMPONENTS.monthDays,
+  everyYear: COMPONENTS.dateTime | COMPONENTS.monthDays | COMPONENTS.months,
+};
